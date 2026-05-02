f915e788eddc57f42a77cd49e98f7b3f7d7fb866
Anvesha Garg <anveshamanvigarg.13@gmail.com>
2026-05-02 12:37:21 +0530

    frontend

---
+++ notification_app_fe/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "30px", fontFamily: "Arial" }}>
+      <h1>Campus Notifications</h1>
+      <p>Frontend is running successfully.</p>
+    </main>
+  );
+}
